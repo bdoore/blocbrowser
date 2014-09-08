397a660d734142b03cd0a05a2930d8044fec2eee
Brian Doore <doore.b@gmail.com>
2014-09-08 14:56:31 -0500

    Initial Commit

---
+++ BlocBrowser/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  BlocBrowser
+//
+//  Created by Brian Doore on 9/8/14.
+//  Copyright (c) 2014 Brian Doore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlocBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Brian Doore on 9/8/14.
+//  Copyright (c) 2014 Brian Doore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLCAppDelegate class]));
+    }
+}
