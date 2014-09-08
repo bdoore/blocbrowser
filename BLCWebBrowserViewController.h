54df0be0a0f81aaf216b6de85a143fedb727624a
Brian Doore <doore.b@gmail.com>
2014-09-08 17:26:13 -0500

    Functional web browser

---
+++ BLCWebBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLCWebBrowserViewController.h
+//  BlocBrowser
+//
+//  Created by Brian Doore on 9/8/14.
+//  Copyright (c) 2014 Brian Doore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCWebBrowserViewController : UIViewController
+
+@end
